f6b4f15d24c9e8f16e5b15ac0fdd659d89c49b85
Jorge <attiejorge89@gmail.com>
2025-08-24 11:16:12 -0300

    Increase background zoom level for the first slide in HeroSection component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -90,7 +90,7 @@ const HeroSection = () => {
                       : "center top 75px"
                     : "center top 75px",
                 backgroundSize:
-                  window.innerWidth < 640 && index === 0 ? "350%" : "cover",
+                  window.innerWidth < 640 && index === 0 ? "420%" : "cover",
               }}
             />
 
